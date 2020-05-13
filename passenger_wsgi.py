ecd12fff40aedfd83f4c367e2c0e5e9e1a9d599f
Herbert <blokdata@herbert.ugent.be>
2020-05-13 16:59:53 +0200

    Fix deployment

---
+++ passenger_wsgi.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+INTERP = os.path.expanduser("~/venv/bin/python3")
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+sys.path.append(os.getcwd())
+from blokdata.app import app as application
+
+# For running on the server with passenger etc
+if __name__ == "__main__":
+    application.run()
