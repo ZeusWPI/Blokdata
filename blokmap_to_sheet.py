8a010266a050b5a6c1cf2c42050e2b4d7a70a103
maxime <maxime@xps9300.kelder.local>
2022-06-01 17:07:26 +0200

    once you go black ⚫

--- blokmap_to_sheet.py
+++ blokmap_to_sheet.py
@@ -6,21 +6,25 @@ response = requests.get("https://blok.ugent.be/data.json")
 data = response.json()
 
 for location in data:
-    print("\t".join([
-        str(location["geometry"]["coordinates"][1]),
-        str(location["geometry"]["coordinates"][0]),
-        str(location["properties"]["name"]),
-        str(location["properties"]["address"]),
-        str(location["properties"]["capacity"]),
-        str(location["properties"]["period"]["start"]),
-        str(location["properties"]["period"]["end"]),
-        str(location["properties"]["hours"]["monday"]),
-        str(location["properties"]["hours"]["tuesday"]),
-        str(location["properties"]["hours"]["wednesday"]),
-        str(location["properties"]["hours"]["thursday"]),
-        str(location["properties"]["hours"]["friday"]),
-        str(location["properties"]["hours"]["saturday"]),
-        str(location["properties"]["hours"]["sunday"]),
-        str(location["properties"]["extra"]),
-        str(location["properties"]["type"])
-    ]))
+    print(
+        "\t".join(
+            [
+                str(location["geometry"]["coordinates"][1]),
+                str(location["geometry"]["coordinates"][0]),
+                str(location["properties"]["name"]),
+                str(location["properties"]["address"]),
+                str(location["properties"]["capacity"]),
+                str(location["properties"]["period"]["start"]),
+                str(location["properties"]["period"]["end"]),
+                str(location["properties"]["hours"]["monday"]),
+                str(location["properties"]["hours"]["tuesday"]),
+                str(location["properties"]["hours"]["wednesday"]),
+                str(location["properties"]["hours"]["thursday"]),
+                str(location["properties"]["hours"]["friday"]),
+                str(location["properties"]["hours"]["saturday"]),
+                str(location["properties"]["hours"]["sunday"]),
+                str(location["properties"]["extra"]),
+                str(location["properties"]["type"]),
+            ]
+        )
+    )
